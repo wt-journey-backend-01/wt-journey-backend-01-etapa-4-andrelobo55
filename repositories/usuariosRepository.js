e93760c665394b5ca3a66606ef880868b1a974d9
andrelobo55 <loboandre55@gmail.com>
2025-08-23 14:19:15 -0300

    feat: creating userRepository.js

---
+++ repositories/usuariosRepository.js
@@ -0,0 +1,33 @@
+const db = require('../db/db');
+
+async function create(user) {
+    const created = await db('usuarios').insert(user).returning("*").first();
+    return created;
+}
+
+async function readAll() {
+    const result = await db('usuarios').select("*");
+    return result;
+}
+
+async function readById(id) {
+    const result = await db('usuarios').where({ id: id }).select("*").first();
+    return result;
+}
+
+async function update(id, user) {
+    const updated = await db('usuarios').where({ id: id }).update(user).returning("*").first();
+    return updated;
+}
+
+async function patch(id, fields) {
+    const updated = await db('usuarios').where({ id: id }).update(fields).returning("*").first();
+    return updated;
+}
+
+async function remove(id) {
+    const deleted = await db('usuarios').where({ id: id }).del();
+    return deleted;
+}
+
+module.exports = { create, readAll, readById, update, patch, remove };
