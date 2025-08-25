97eca61663f6079269e30ba39acddeaa3c9e2c54
andrelobo55 <loboandre55@gmail.com>
2025-08-25 17:03:13 -0300

    fix: adding module.exports

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -2,6 +2,8 @@ class APIError extends Error {
     constructor(status, message){
         super(message);
         this.status = status;
-        this.name = "API Error";
+        this.name = "APIError";
     }
 }
+
+module.exports = APIError;
